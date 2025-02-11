59e37d945bc4d3b001e40d14c3b5492f743677b5
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-11 19:13:59 +0300

    [02.15] refactored access modifiers for Request.java

--- src/main/java/com/example/teamcity/api/requests/Request.java
+++ src/main/java/com/example/teamcity/api/requests/Request.java
@@ -5,8 +5,8 @@ import io.restassured.specification.RequestSpecification;
 
 public class Request {
     // Request is a class describing changing request's parameters: specification, endpoint (relative URL, model)
-    private final RequestSpecification spec;
-    private final Endpoint endpoint;
+    protected final RequestSpecification spec;
+    protected final Endpoint endpoint;
 
     public Request(RequestSpecification spec, Endpoint endpoint) {
         this.spec = spec;
