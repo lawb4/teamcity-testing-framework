d515f0b69b78d4e3d3f7a5b936a4fde334442832
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-11 18:30:29 +0300

    [02.14] add enum, models, CrudInterface, basic Request

---
+++ src/main/java/com/example/teamcity/api/requests/Request.java
@@ -0,0 +1,15 @@
+package com.example.teamcity.api.requests;
+
+import com.example.teamcity.api.enums.Endpoint;
+import io.restassured.specification.RequestSpecification;
+
+public class Request {
+    // Request is a class describing changing request's parameters: specification, endpoint (relative URL, model)
+    private final RequestSpecification spec;
+    private final Endpoint endpoint;
+
+    public Request(RequestSpecification spec, Endpoint endpoint) {
+        this.spec = spec;
+        this.endpoint = endpoint;
+    }
+}
