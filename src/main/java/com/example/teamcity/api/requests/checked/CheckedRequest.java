644a7d832cb9443cb6d056a61a69cb3f035062a6
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-11 19:12:57 +0300

    [02.15] add checked/unchecked request classes

---
+++ src/main/java/com/example/teamcity/api/requests/checked/CheckedRequest.java
@@ -0,0 +1,51 @@
+package com.example.teamcity.api.requests.checked;
+
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.BaseModel;
+import com.example.teamcity.api.requests.CrudInterface;
+import com.example.teamcity.api.requests.Request;
+import com.example.teamcity.api.requests.unchecked.UncheckedRequest;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+
+@SuppressWarnings("unchecked")
+public class CheckedRequest<T extends BaseModel> extends Request implements CrudInterface {
+    private UncheckedRequest uncheckedRequest;
+
+    public CheckedRequest(RequestSpecification spec, Endpoint endpoint) {
+        super(spec, endpoint);
+        this.uncheckedRequest = new UncheckedRequest(spec, endpoint);
+    }
+
+    @Override
+    public T create(BaseModel model) {
+        return (T) uncheckedRequest
+                .create(model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public T read(String id) {
+        return (T) uncheckedRequest
+                .read(id)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public T update(String id, BaseModel model) {
+        return (T) uncheckedRequest
+                .update(id, model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+    }
+
+    @Override
+    public Object delete(String id) {
+        return uncheckedRequest
+                .delete(id)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
+    }
+}
