c4065094920a01d3d4829ed15a115c30b2570292
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-14 13:06:02 +0300

    [02.20] add userCreatesTwoBuildTypesWithTheSameIdTest

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -5,7 +5,10 @@ import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
+import com.example.teamcity.api.requests.unchecked.UncheckedRequest;
 import com.example.teamcity.api.spec.Specifications;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -47,11 +50,23 @@ public class BuildTypeTest extends BaseApiTest {
     @DisplayName("User should not be able to create two build types with the same id")
     @Tags({@Tag("Negative"), @Tag("CRUD")})
     public void userCreatesTwoBuildTypesWithTheSameIdTest() {
-        step("Create user");
-        step("Create project by user");
-        step("Create buildType1 for project by user");
-        step("Create buildType2 with the same id as buildType1 for project by user");
-        step("Check buildType2 was not created with bad request code");
+        var user = generate(User.class);
+
+        superUserCheckedRequests.getRequest(USERS).create(user);
+        var userCheckedRequests = new CheckedRequests(Specifications.authSpec(user));
+
+        var project = generate(Project.class);
+        project = userCheckedRequests.<Project>getRequest(PROJECTS).create(project);
+
+        var buildType1 = generate(Collections.singletonList(project), BuildType.class);
+        var buildType2 = generate(Collections.singletonList(project), BuildType.class,
+                buildType1.getId());
+
+        userCheckedRequests.getRequest(BUILD_TYPES).create(buildType1);
+        new UncheckedRequest(Specifications.authSpec(user), BUILD_TYPES)
+                .create(buildType2)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("The build configuration / template ID \"%s\" is already used by another configuration or template".formatted(buildType1.getId())));
     }
 
     @Test
