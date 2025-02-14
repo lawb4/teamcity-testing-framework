c02b8103ac689d202475055a2f0d9e6077c1d693
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-14 09:57:20 +0300

    [02.18] add soft-assertions support

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,4 +1,19 @@
 package com.example.teamcity;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class BaseTest {
+    protected SoftAssertions softly;
+
+    @BeforeEach
+    public void beforeTest() {
+        softly = new SoftAssertions();
+    }
+
+    @AfterEach
+    public void afterTest() {
+        softly.assertAll();
+    }
 }
