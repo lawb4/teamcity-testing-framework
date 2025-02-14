794ab6c2f6719ddc0111445e1a278d2bb93d54e6
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-14 09:57:42 +0300

    [02.18] refactored BuildTypeTest

--- src/test/java/com/example/teamcity/api/BuildTypeTest.java
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -2,6 +2,8 @@ package com.example.teamcity.api;
 
 
 import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.models.BuildType;
+import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.api.requests.checked.CheckedRequest;
 import com.example.teamcity.api.spec.Specifications;
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 import static io.qameta.allure.Allure.step;
@@ -22,13 +26,35 @@ public class BuildTypeTest extends BaseApiTest {
         var user = generate(User.class);
 
         step("Create user", () -> {
-
             var requester = new CheckedRequest<User>(Specifications.superUserAuthSpec(), Endpoint.USERS);
             requester.create(user);
         });
-        step("Create project by user");
-        step("Create buildType for project by user");
-        step("Check buildType was created successfully with correct data");
+
+        var project = generate(Project.class);
+        AtomicReference<String> projectId = new AtomicReference<>("");
+
+        step("Create project by user", () -> {
+            var requester = new CheckedRequest<Project>(Specifications.authSpec(user), Endpoint.PROJECTS);
+            projectId.set(requester.create(project).getId());
+        });
+
+        var buildType = generate(BuildType.class);
+        buildType.setProject(Project.builder().id(projectId.get()).locator(null).build());
+
+        var requester = new CheckedRequest<BuildType>(Specifications.authSpec(user), Endpoint.BUILD_TYPES);
+        AtomicReference<String> buildTypeId = new AtomicReference<>("");
+
+        step("Create buildType for project by user", () -> {
+            buildTypeId.set(requester.create(buildType).getId());
+        });
+
+        step("Check buildType was created successfully with correct data", () -> {
+            var createdBuildType = requester.read(buildTypeId.get());
+
+            softly.assertThat(buildType.getName())
+                    .as("Build type name is not correct")
+                    .isEqualTo(createdBuildType.getName());
+        });
     }
 
     @Test
