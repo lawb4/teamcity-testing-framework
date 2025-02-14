a7c1af5da2cf04582b789dc66408bc350a9e6e5d
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-14 11:16:34 +0300

    [02.19] add CheckedRequests facade for BaseTest

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,11 +1,14 @@
 package com.example.teamcity;
 
+import com.example.teamcity.api.requests.checked.CheckedRequests;
+import com.example.teamcity.api.spec.Specifications;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
     protected SoftAssertions softly;
+    protected CheckedRequests superUserCheckedRequests = new CheckedRequests(Specifications.superUserAuthSpec());
 
     @BeforeEach
     public void beforeTest() {
