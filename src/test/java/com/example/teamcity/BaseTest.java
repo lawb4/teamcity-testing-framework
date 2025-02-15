e09158fd483b8aef7c7af691569639efcb417e2d
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-15 13:04:45 +0300

    [02.21] refactored BaseTest functionality with test data generation before each test

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -1,18 +1,23 @@
 package com.example.teamcity;
 
+import com.example.teamcity.api.models.TestData;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
+
 public class BaseTest {
     protected SoftAssertions softly;
     protected CheckedRequests superUserCheckedRequests = new CheckedRequests(Specifications.superUserAuthSpec());
+    protected TestData testData;
 
     @BeforeEach
     public void beforeTest() {
         softly = new SoftAssertions();
+        testData = generate();
     }
 
     @AfterEach
