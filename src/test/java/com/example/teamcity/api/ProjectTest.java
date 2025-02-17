7944acb71a5418c0af78ae05f0f08852e7b29929
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-17 17:59:16 +0300

    add 1 positive and 4 negative tests for project creation

--- src/test/java/com/example/teamcity/api/ProjectTest.java
+++ src/test/java/com/example/teamcity/api/ProjectTest.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.api;
 
+import com.example.teamcity.api.generators.RandomData;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
 import com.example.teamcity.api.requests.unchecked.UncheckedRequest;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 import static com.example.teamcity.api.enums.Endpoint.USERS;
+import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
 @Tag("Regression")
 public class ProjectTest extends BaseApiTest {
@@ -24,7 +26,7 @@ public class ProjectTest extends BaseApiTest {
         superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
         var userCheckedRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
 
-        userCheckedRequests.<Project>getRequest(PROJECTS).create(testData.getProject());
+        userCheckedRequests.getRequest(PROJECTS).create(testData.getProject());
 
         var createdProject = userCheckedRequests.<Project>getRequest(PROJECTS)
                 .read(testData.getProject().getId());
@@ -32,6 +34,25 @@ public class ProjectTest extends BaseApiTest {
         softly.assertThat(testData.getProject().getName())
                 .as("Project name is not correct")
                 .isEqualTo(createdProject.getName());
+    }
+
+    @Test
+    @DisplayName("User should be able to create a project with a maximum Project Id length (225 characters)")
+    @Tags({@Tag("Positive"), @Tag("CRUD")})
+    public void userCreatesProjectWithMaxProjectIdLengthTest() {
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+        var userCheckedRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        testData.getProject().setId(RandomData.getString(225));
+
+        userCheckedRequests.getRequest(PROJECTS).create(testData.getProject());
+
+        var createdProject = userCheckedRequests.<Project>getRequest(PROJECTS)
+                .read(testData.getProject().getId());
+
+        softly.assertThat(testData.getProject().getId())
+                .as("Project id is not valid")
+                .isEqualTo(createdProject.getId());
     }
 
     @Test
@@ -49,5 +70,70 @@ public class ProjectTest extends BaseApiTest {
                 .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
                 .body(Matchers.containsString("Project with this name already exists: %s"
                         .formatted(testData.getProject().getName())));
+    }
+
+    @Test
+    @DisplayName("User should not be able to create a project with an empty name")
+    @Tags({@Tag("Negative"), @Tag("CRUD")})
+    public void userCreatesProjectWithEmptyName() {
+        var projectWithEmptyName = generate(Project.class);
+        projectWithEmptyName.setName("");
+
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+        var userCheckedRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        userCheckedRequests.getRequest(PROJECTS).create(testData.getProject());
+        new UncheckedRequest(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(projectWithEmptyName)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("Project name cannot be empty"));
+    }
+
+    @Test
+    @DisplayName("User should not be able to create a project with an empty Project ID")
+    @Tags({@Tag("Negative"), @Tag("CRUD")})
+    public void userCreatesProjectWithEmptyProjectId() {
+        var projectWithEmptyName = generate(Project.class);
+        projectWithEmptyName.setId("");
+
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+        var userCheckedRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        userCheckedRequests.getRequest(PROJECTS).create(testData.getProject());
+        new UncheckedRequest(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(projectWithEmptyName)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString("Project ID must not be empty"));
+    }
+
+    @Test
+    @DisplayName("User should not be able to create a project with a Project ID that starts with a digit")
+    @Tags({@Tag("Negative"), @Tag("CRUD")})
+    public void userCreatesProjectWithProjectIdStartsWithDigit() {
+        var project = testData.getProject();
+        project.setId(RandomData.getStringStartingWithDigit());
+
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+
+        new UncheckedRequest(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(project)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString("Project ID \"%s\" is invalid: starts with non-letter character '%c'. ID should start with a latin letter and contain only latin letters, digits and underscores (at most 225 characters)".formatted(project.getId(), project.getId().charAt(0))));
+    }
+
+    @Test
+    @DisplayName("User should not be able to create a project with a Project Id length that exceed maximum value (>= 226 characters)")
+    @Tags({@Tag("Negative"), @Tag("CRUD")})
+    public void userCreatesProjectWithExceedingProjectIdLengthTest() {
+        var project = testData.getProject();
+        int projectIdLength = 226;
+        project.setId(RandomData.getString(projectIdLength));
+
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+
+        new UncheckedRequest(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(project)
+                .then().assertThat().statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
+                .body(Matchers.containsString("Project ID \"%s\" is invalid: it is %d characters long while the maximum length is 225. ID should start with a latin letter and contain only latin letters, digits and underscores (at most 225 characters)".formatted(project.getId(), projectIdLength)));
     }
 }
