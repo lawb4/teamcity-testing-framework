5b2d9323efa87c468c90c247f745488cb47abfcc
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-17 12:48:03 +0300

    add negative test for project creation with same id

--- src/test/java/com/example/teamcity/api/ProjectTest.java
+++ src/test/java/com/example/teamcity/api/ProjectTest.java
@@ -2,7 +2,10 @@ package com.example.teamcity.api;
 
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.api.requests.checked.CheckedRequests;
+import com.example.teamcity.api.requests.unchecked.UncheckedRequest;
 import com.example.teamcity.api.spec.Specifications;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
@@ -29,5 +32,22 @@ public class ProjectTest extends BaseApiTest {
         softly.assertThat(testData.getProject().getName())
                 .as("Project name is not correct")
                 .isEqualTo(createdProject.getName());
+    }
+
+    @Test
+    @DisplayName("User should not be able to create two projects with the same name")
+    @Tags({@Tag("Negative"), @Tag("CRUD")})
+    public void userCreatesTwoProjectsWithSameNameTest() {
+        var projectWithSameName = testData.getProject();
+
+        superUserCheckedRequests.getRequest(USERS).create(testData.getUser());
+        var userCheckedRequests = new CheckedRequests(Specifications.authSpec(testData.getUser()));
+
+        userCheckedRequests.getRequest(PROJECTS).create(testData.getProject());
+        new UncheckedRequest(Specifications.authSpec(testData.getUser()), PROJECTS)
+                .create(projectWithSameName)
+                .then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(Matchers.containsString("Project with this name already exists: %s"
+                        .formatted(testData.getProject().getName())));
     }
 }
