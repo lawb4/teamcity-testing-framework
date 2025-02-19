47884c0a5b7617add7e9e312a866e7e1222ca799
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-19 17:38:00 +0300

    ignore redundant null fields when sending requests

--- src/main/java/com/example/teamcity/api/models/Project.java
+++ src/main/java/com/example/teamcity/api/models/Project.java
@@ -2,6 +2,7 @@ package com.example.teamcity.api.models;
 
 import com.example.teamcity.api.annotations.Random;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,6 @@ public class Project extends BaseModel {
     private String id;
     @Random
     private String name;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String locator;
 }
