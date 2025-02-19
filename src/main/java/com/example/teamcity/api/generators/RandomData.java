f1c11b3dfba82b99327bd6c395e20216e1ae5ff8
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-19 15:17:26 +0300

    added helper methods for project tests

--- src/main/java/com/example/teamcity/api/generators/RandomData.java
+++ src/main/java/com/example/teamcity/api/generators/RandomData.java
@@ -2,6 +2,8 @@ package com.example.teamcity.api.generators;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.util.stream.Stream;
+
 public final class RandomData {
     private static final String TEST_PREFIX = "test_";
     private static final int MAX_LENGTH = 10;
@@ -16,7 +18,27 @@ public final class RandomData {
         );
     }
 
+    public static String getStringEndsWithSpecialCharacter(char specialCharacter) {
+        return TEST_PREFIX + RandomStringUtils.randomAlphabetic(MAX_LENGTH) + specialCharacter;
+    }
+
+    public static String getStringStartsWithSpecialCharacter(char specialCharacter) {
+        return specialCharacter + TEST_PREFIX + RandomStringUtils.randomAlphabetic(MAX_LENGTH);
+    }
+
+    public static String getStringWithCyrillic() {
+        return TEST_PREFIX + RandomStringUtils.random(MAX_LENGTH, 0x0410, 0x0450, false, false);
+    }
+
     public static String getStringStartingWithDigit() {
         return RandomStringUtils.randomNumeric(1) + TEST_PREFIX + RandomStringUtils.randomAlphabetic(MAX_LENGTH);
+    }
+
+    public static Stream<String> invalidCharacters() {
+        return Stream.of("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?", "_");
+    }
+
+    public static Stream<String> invalidCharactersExceptUnderscore() {
+        return Stream.of("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?");
     }
 }
