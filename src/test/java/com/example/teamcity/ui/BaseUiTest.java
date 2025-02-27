ad8fb91bc6016eeda107f200386dd22fb66c460a
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-27 16:40:57 +0300

    [03.08] configure selenide and BaseUiTest

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -1,4 +1,31 @@
 package com.example.teamcity.ui;
 
-public class BaseUiTest {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.config.Config;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Map;
+
+public class BaseUiTest extends BaseTest {
+
+    @BeforeEach
+    public void setupUiTest() {
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        Configuration.remote = Config.getProperty("remote");
+        Configuration.browserSize = Config.getProperty("browserSize");
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", true,
+                        "enableLog", true)
+        );
+    }
+
+    @AfterEach
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
 }
