b2f111e571a4eecbba4bf33710fb192082d48919
Yahor Labanau <yahorlabanau@gmail.com>
2025-02-28 15:05:24 +0300

    [03.11] implement wait for successful baseCreateForm message

--- src/main/java/com/example/teamcity/ui/pages/admin/CreateBasePage.java
+++ src/main/java/com/example/teamcity/ui/pages/admin/CreateBasePage.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.ui.pages.admin;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.SelenideElement;
 import com.example.teamcity.ui.pages.BasePage;
@@ -12,9 +13,11 @@ public abstract class CreateBasePage extends BasePage {
     protected SelenideElement inputUrl = $("#url");
     protected SelenideElement inputBuildTypeName = $("#buildTypeName");
     protected SelenideElement submitButton = $(Selectors.byAttribute("value", "Proceed"));
+    protected SelenideElement connectionSuccessfulMessage = $(".connectionSuccessful");
 
     protected void baseCreateForm(String url) {
         inputUrl.val(url);
         submitButton.click();
+        connectionSuccessfulMessage.should(Condition.appear, BASE_WAITING);
     }
 }
