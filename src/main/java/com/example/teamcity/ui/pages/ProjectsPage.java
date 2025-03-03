0a6c3e226a36a1bdeffa59ea8198d033b2d42ab2
Yahor Labanau <yahorlabanau@gmail.com>
2025-03-03 14:33:59 +0300

    [03.12] implemented PageElement and improved ui test

--- src/main/java/com/example/teamcity/ui/pages/ProjectsPage.java
+++ src/main/java/com/example/teamcity/ui/pages/ProjectsPage.java
@@ -1,5 +1,23 @@
 package com.example.teamcity.ui.pages;
 
-public class ProjectsPage extends BasePage {
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.example.teamcity.ui.elements.ProjectElement;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+public class ProjectsPage extends BasePage {
+    private static final String PROJECTS_URL = "/favourite/projects";
+
+    private ElementsCollection projectElements = $$("div[class*='Subproject__container']");
+
+    public static ProjectsPage open() {
+        return Selenide.open(PROJECTS_URL, ProjectsPage.class);
+    }
+
+    public List<ProjectElement> getProjects() {
+        return generatePageElements(projectElements, ProjectElement::new);
+    }
 }
