5122be1db2b37325842249e142409d77a9a33cc6
Yahor Labanau <yahorlabanau@gmail.com>
2025-03-25 18:53:20 +0300

    [04hw] road to 5 "passed" runs - 16

--- src/test/java/com/example/teamcity/ui/CreateBuildTypeTest.java
+++ src/test/java/com/example/teamcity/ui/CreateBuildTypeTest.java
@@ -1,6 +1,6 @@
 package com.example.teamcity.ui;
 
-import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.BuildType;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.ui.pages.ProjectPage;
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.example.teamcity.api.enums.Endpoint.BUILD_TYPES;
+import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
 
 @Tag("Regression")
@@ -27,7 +29,7 @@ public class CreateBuildTypeTest extends BaseUiTest {
                 .createForm(REPO_URL_FOR_PROJECT)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS)
+        var createdProject = superUserCheckedRequests.<Project>getRequest(PROJECTS)
                 .read("name:" + testData.getProject().getName());
 
         var newBuildTypeTestData = generate().getBuildType();
@@ -36,9 +38,11 @@ public class CreateBuildTypeTest extends BaseUiTest {
                 .createForm(REPO_URL_FOR_BUILD_TYPE)
                 .setupBuildType(newBuildTypeTestData.getName());
 
-        var createdBuildType = superUserCheckedRequests.<BuildType>getRequest(Endpoint.BUILD_TYPES)
+        var createdBuildType = superUserCheckedRequests.<BuildType>getRequest(BUILD_TYPES)
                 .read("name:" + newBuildTypeTestData.getName());
         softly.assertThat(createdBuildType).isNotNull();
+
+        TestDataStorage.getStorage().addCreatedEntity(BUILD_TYPES, createdBuildType);
 
         var foundBuildTypes = ProjectPage.open(createdProject.getId())
                 .getBuildTypes().stream()
@@ -57,7 +61,9 @@ public class CreateBuildTypeTest extends BaseUiTest {
                 .createForm(REPO_URL_FOR_PROJECT)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS).read("name:" + testData.getProject().getName());
+        var createdProject = superUserCheckedRequests.<Project>getRequest(PROJECTS).read("name:" + testData.getProject().getName());
+
+        TestDataStorage.getStorage().addCreatedEntity(PROJECTS, createdProject);
 
         CreateBuildTypePage.open(createdProject.getId())
                 .createForm(REPO_URL_FOR_BUILD_TYPE)
@@ -75,7 +81,9 @@ public class CreateBuildTypeTest extends BaseUiTest {
                 .createForm(REPO_URL_FOR_PROJECT)
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
-        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS).read("name:" + testData.getProject().getName());
+        var createdProject = superUserCheckedRequests.<Project>getRequest(PROJECTS).read("name:" + testData.getProject().getName());
+
+        TestDataStorage.getStorage().addCreatedEntity(PROJECTS, createdProject);
 
         CreateBuildTypePage.open(createdProject.getId())
                 .createForm(REPO_URL_FOR_BUILD_TYPE)
--- src/test/java/com/example/teamcity/ui/CreateProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -1,7 +1,7 @@
 package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Condition;
-import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.ui.pages.ProjectPage;
 import com.example.teamcity.ui.pages.ProjectsPage;
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.Allure.step;
+import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 
 @Tag("Regression")
 public class CreateProjectTest extends BaseUiTest {
@@ -29,8 +29,10 @@ public class CreateProjectTest extends BaseUiTest {
                 .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
 
         // Check API state (correct state of sent data from UI to API level)
-        var createdProject = superUserCheckedRequests.<Project>getRequest(Endpoint.PROJECTS).read("name:" + testData.getProject().getName());
+        var createdProject = superUserCheckedRequests.<Project>getRequest(PROJECTS).read("name:" + testData.getProject().getName());
         softly.assertThat(createdProject).isNotNull();
+
+        TestDataStorage.getStorage().addCreatedEntity(PROJECTS, createdProject);
 
         // Check UI state (correct data processing and display on UI level)
         ProjectPage.open(createdProject.getId())
@@ -41,25 +43,25 @@ public class CreateProjectTest extends BaseUiTest {
         softly.assertThat(foundProjects).isTrue();
     }
 
-    @Test
-    @DisplayName("User should not be able to create a project without name")
-    @Tag("Negative")
-    public void userCreatesProjectWithoutName() {
-        // Prepare test environment and data
-        step("Login as user");
-        step("Check number of projects");
-
-        // Interaction with UI
-        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
-        step("Send all project parameters (repository URL)");
-        step("Click Proceed");
-        step("Set Project Name");
-        step("Click Proceed");
-
-        // Check API state (correct state of sent data from UI to API level)
-        step("Check that number of projects did not change");
-
-        // Check UI state (correct data processing and display on UI level)
-        step("Check that following error appears: 'Project name must not be empty'");
-    }
+//    @Test
+//    @DisplayName("User should not be able to create a project without name")
+//    @Tag("Negative")
+//    public void userCreatesProjectWithoutName() {
+//        // Prepare test environment and data
+//        step("Login as user");
+//        step("Check number of projects");
+//
+//        // Interaction with UI
+//        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
+//        step("Send all project parameters (repository URL)");
+//        step("Click Proceed");
+//        step("Set Project Name");
+//        step("Click Proceed");
+//
+//        // Check API state (correct state of sent data from UI to API level)
+//        step("Check that number of projects did not change");
+//
+//        // Check UI state (correct data processing and display on UI level)
+//        step("Check that following error appears: 'Project name must not be empty'");
+//    }
 }
