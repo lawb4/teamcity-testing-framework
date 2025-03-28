d7b75ede81671112daf376f595ec1e69baf909b4
Yahor Labanau <yahorlabanau@gmail.com>
2025-03-28 12:37:55 +0300

    [04hw] make @Step annotation work (stable-1)

--- src/main/java/com/example/teamcity/api/requests/checked/CheckedRequest.java
+++ src/main/java/com/example/teamcity/api/requests/checked/CheckedRequest.java
@@ -21,69 +21,73 @@ public class CheckedRequest<T extends BaseModel> extends Request implements Crud
     }
 
     @Override
-    //@Step("Create {0}")
+    //@Step("!!! Create {model.getClass().getSimpleName()}")
+    @Step("!!! Create {0}")
     public T create(BaseModel model) {
-//        var createdModel = (T) uncheckedRequest
-//                .create(model)
-//                .then().assertThat().statusCode(HttpStatus.SC_OK)
-//                .extract().as(endpoint.getModelClass());
-//
-//        TestDataStorage.getStorage().addCreatedEntity(endpoint, createdModel);
-//        return createdModel;
+        var createdModel = (T) uncheckedRequest
+                .create(model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+
+        TestDataStorage.getStorage().addCreatedEntity(endpoint, createdModel);
+        return createdModel;
         // Use Allure lifecycle to explicitly create a step
-        return Allure.step("Create " + model.getClass().getSimpleName(), () -> {
-            var createdModel = (T) uncheckedRequest
-                    .create(model)
-                    .then().assertThat().statusCode(HttpStatus.SC_OK)
-                    .extract().as(endpoint.getModelClass());
-
-            TestDataStorage.getStorage().addCreatedEntity(endpoint, createdModel);
-            return createdModel;
-        });
+//        return Allure.step("Create " + model.getClass().getSimpleName(), () -> {
+//            var createdModel = (T) uncheckedRequest
+//                    .create(model)
+//                    .then().assertThat().statusCode(HttpStatus.SC_OK)
+//                    .extract().as(endpoint.getModelClass());
+//
+//            TestDataStorage.getStorage().addCreatedEntity(endpoint, createdModel);
+//            return createdModel;
+//        });
     }
 
     @Override
-    //@Step("Read {endpoint.modelClass.simpleName} with locator: {locator}")
+    //@Step("!!! Read {endpoint.modelClass.simpleName} with locator: {locator}")
+    @Step("Read {endpoint.modelClass.simpleName} with locator: {0}")
     public T read(String locator) {
-//        return (T) uncheckedRequest
-//                .read(locator)
-//                .then().assertThat().statusCode(HttpStatus.SC_OK)
-//                .extract().as(endpoint.getModelClass());
-        return Allure.step("Read " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
-            return (T) uncheckedRequest
-                    .read(locator)
-                    .then().assertThat().statusCode(HttpStatus.SC_OK)
-                    .extract().as(endpoint.getModelClass());
-        });
+        return (T) uncheckedRequest
+                .read(locator)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+//        return Allure.step("Read " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
+//            return (T) uncheckedRequest
+//                    .read(locator)
+//                    .then().assertThat().statusCode(HttpStatus.SC_OK)
+//                    .extract().as(endpoint.getModelClass());
+//        });
     }
 
     @Override
-    //@Step("Update {endpoint.modelClass.simpleName} with locator: {locator} and data: {model}")
+    //@Step("!!! Update {endpoint.modelClass.simpleName} with locator: {locator} and data: {model}")
+    @Step("Update {1} with locator: {0}")
     public T update(String locator, BaseModel model) {
-//        return (T) uncheckedRequest
-//                .update(locator, model)
-//                .then().assertThat().statusCode(HttpStatus.SC_OK)
-//                .extract().as(endpoint.getModelClass());
-        return Allure.step("Update " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
-            return (T) uncheckedRequest
-                    .read(locator)
-                    .then().assertThat().statusCode(HttpStatus.SC_OK)
-                    .extract().as(endpoint.getModelClass());
-        });
+        return (T) uncheckedRequest
+                .update(locator, model)
+                .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().as(endpoint.getModelClass());
+//        return Allure.step("Update " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
+//            return (T) uncheckedRequest
+//                    .read(locator)
+//                    .then().assertThat().statusCode(HttpStatus.SC_OK)
+//                    .extract().as(endpoint.getModelClass());
+//        });
     }
 
     @Override
     //@Step("Delete {endpoint.modelClass.simpleName} with locator: {locator}")
+    @Step("Delete {endpoint.modelClass.simpleName} with locator: {0}")
     public Object delete(String locator) {
-//        return uncheckedRequest
-//                .delete(locator)
-//                .then().assertThat().statusCode(HttpStatus.SC_NO_CONTENT)
-//                .extract().asString();
-        return Allure.step("Delete " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
-            return (T) uncheckedRequest
-                    .read(locator)
-                    .then().assertThat().statusCode(HttpStatus.SC_OK)
-                    .extract().as(endpoint.getModelClass());
-        });
+        return uncheckedRequest
+                .delete(locator)
+                .then().assertThat().statusCode(HttpStatus.SC_NO_CONTENT)
+                .extract().asString();
+//        return Allure.step("Delete " + endpoint.getModelClass().getSimpleName() + " with locator: " + locator, () -> {
+//            return (T) uncheckedRequest
+//                    .read(locator)
+//                    .then().assertThat().statusCode(HttpStatus.SC_OK)
+//                    .extract().as(endpoint.getModelClass());
+//        });
     }
 }
